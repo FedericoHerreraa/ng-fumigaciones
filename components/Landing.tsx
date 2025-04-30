dc7e4beb3d74a323642010602b57e34586e9dee1
Federico Herrera <fede.juan.herrera@gmail.com>
2025-04-30 15:00:41 -0300

    correcing some details

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -3,7 +3,7 @@ import backgroundImage from '@/images/fumigacion2.jpeg'
 
 export const Landing = () => {
     return (
-        <div className="h-[80vh] bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage.src})` }}>
+        <div className="h-[calc(100vh-176px)] bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage.src})` }}>
             <div className="absolute left-1/2 -translate-x-1/2 top-1/2 z-10">
                 <div className='bg-gradient-to-r from-green-200 to-green-200/50 px-10 py-4 rounded-xs'>
                     <h1 className='text-3xl font-serif'>NG Desinfecciones</h1>
