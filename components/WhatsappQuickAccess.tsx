cca3dd59cbcacaa483d9c0b57bcf9270848c5beb
Federico Herrera <fede.juan.herrera@gmail.com>
2025-06-17 10:18:54 -0300

    some changes

--- components/WhatsappQuickAccess.tsx
+++ components/WhatsappQuickAccess.tsx
@@ -7,7 +7,7 @@ export const WhatsappQuickAccess = () => {
     const message = 'Hola buenas, tengo una consulta...'
 
     return (
-        <div className="fixed md:bottom-5 md:right-5 bottom-2 right-2 z-50 cursor-pointer">
+        <div className="fixed md:bottom-5 md:right-5 bottom-2 right-2 z-50 cursor-pointer hover:scale-110 transition-all duration-300">
             <a href={`https://api.whatsapp.com/send?phone=${phone}&text=${message}`} target="_blank">
                 <Image
                     src={whatsappIcon}
