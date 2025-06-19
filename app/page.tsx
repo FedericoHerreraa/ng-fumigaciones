48eea4d51807596e241e4d4fc5da97c3d67853be
CamilaAznar <aznarcami@gmail.com>
2025-06-19 13:45:54 -0300

    resultados

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import { CTA } from "@/components/CTA";
 import { AboutUsComponent } from "@/components/AboutUsComponent";
 import { Services } from "@/components/Services";
 import { FAQs } from "@/components/FAQs";
+import { Results } from "@/components/Results";
 
 export default function Home() {
   return (
     <>
       <Landing />
       <CTA />
+      <Results />
       <Services />
       <FAQs />
       <AboutUsComponent />
